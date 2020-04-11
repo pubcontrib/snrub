ba5e3b1f7ac759518caedd3ee8aae531dfa735b3
Jason Kaiser <wareification@programmer.net>
2020-04-11 11:59:24 -0500

    Added a shell script for testing the behavior of the project.

---
+++ test/run.sh
@@ -0,0 +1,100 @@
+#!/bin/sh
+execute_text()
+{
+    text=$1
+
+    echo $text
+
+    output=`snrub -t "$text"`
+    exit_code=$?
+}
+
+pass()
+{
+    text=$1
+
+    execute_text "$text"
+
+    if [ $exit_code -ne 0 ]
+    then
+        echo "Error!"
+        exit 1
+    fi
+}
+
+fail()
+{
+    text=$1
+
+    execute_text "$text"
+
+    if [ $exit_code -eq 0 ]
+    then
+        echo "Error!"
+        exit 1
+    fi
+}
+
+# Null
+pass '?'
+
+# Number
+pass '#0#'
+pass '#1#'
+pass '#10#'
+pass '#01#'
+pass '#-1#'
+
+# String
+pass '"word"'
+pass '"more than one word"'
+
+# Comment
+pass '~("comments")'
+fail '~'
+fail '~()'
+
+# Value
+pass '<("key")'
+fail '<'
+fail '<()'
+
+# Assign
+pass '>("null" ?)'
+pass '>("number" #100#)'
+pass '>("string" "one hundred")'
+fail '>'
+fail '>()'
+fail '>("key")'
+
+# Add
+pass '+(#10# #5#)'
+fail '+'
+fail '+()'
+fail '+(#5#)'
+
+# Subtract
+pass '-(#10# #5#)'
+fail '-'
+fail '-()'
+fail '-(#5#)'
+
+# Multiply
+pass '*(#10# #5#)'
+fail '*'
+fail '*()'
+fail '*(#5#)'
+
+# Divide
+pass '/(#10# #5#)'
+fail '/'
+fail '/()'
+fail '/(#5#)'
+
+# Comments
+pass '~("comments")'
+fail '~'
+fail '~()'
+
+# Errors
+fail '/(#1# #0#)'
