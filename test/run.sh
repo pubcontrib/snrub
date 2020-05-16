598ec70789e150d56497603b54cd5849fd65c0ed
Jason Kaiser <wareification@programmer.net>
2020-05-16 16:37:42 -0500

    Added a test case to verify a variable can change from a string to a number.

--- test/run.sh
+++ test/run.sh
@@ -92,6 +92,7 @@ pass '(">" "number" #1#) ("<" "number")' '#1#'
 pass '(">" "string" "word") ("<" "string")' '"word"'
 pass '(">" "key" "before") (">" "key" "after") ("<" "key")' '"after"'
 pass '(">" "key" "before") (">" "key" ?) ("<" "key")' '?'
+pass '(">" "key" "before") (">" "key" #1#) ("<" "key")' '#1#'
 fail '("<")' '#4#'
 
 # Assign
