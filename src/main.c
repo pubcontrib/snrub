3757b98d7db27b9e2ef593a7325196e621453e08
Jason Kaiser <wareification@programmer.net>
2020-05-31 11:49:58 -0500

    Updated program version to v0.16.0.

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 #include "common.h"
 
 #define PROGRAM_NAME "snrub"
-#define PROGRAM_VERSION "v0.15.0"
+#define PROGRAM_VERSION "v0.16.0"
 
 static int run_script(char *document);
 static char *read_file(char *path, size_t limit);
