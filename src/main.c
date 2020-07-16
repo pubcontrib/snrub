a8e52b4dc541ca55021409da3602d26ae5ddffb0
Jason Kaiser <wareification@programmer.net>
2020-07-16 14:42:59 -0500

    Updated program version to v0.27.0.

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@
 #include "common.h"
 
 #define PROGRAM_NAME "snrub"
-#define PROGRAM_VERSION "v0.26.0"
+#define PROGRAM_VERSION "v0.27.0"
 
 static int complete_script(char *document);
 static int apply_script(char *document, object_t *objects);
