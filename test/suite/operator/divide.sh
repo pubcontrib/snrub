b8757366f5bed40f7f1b57cbdc05a310c78f35cb
Jason Kaiser <wareification@programmer.net>
2020-08-14 15:04:20 -0500

    Ordered a few missed error test suites.

--- test/suite/operator/divide.sh
+++ test/suite/operator/divide.sh
@@ -11,14 +11,14 @@ pass '("/" #-10# #-6#)' '#1#'
 pass '("/" #0# #1#)' '#0#'
 pass '("/" #6# #10#)' '#0#'
 
-# Arithmetic Error
-fail '("/" #1# #0#)' '#5#'
-
 # Argument Error
 fail '("/")' '#4#'
 fail '("/" #5#)' '#4#'
 fail '("/" ? ?)' '#4#'
 fail '("/" "word" "word")' '#4#'
+
+# Arithmetic Error
+fail '("/" #1# #0#)' '#5#'
 
 # Error Propogation
 fail '("/" ("#" "text") #5#)' '#3#'
