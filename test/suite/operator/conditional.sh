82720fd534c92410fdef8ea3be08e6964cc5d014
Jason Kaiser <wareification@programmer.net>
2020-08-22 14:56:11 -0500

    Added test cases for advanced type combinations.

--- test/suite/operator/conditional.sh
+++ test/suite/operator/conditional.sh
@@ -5,13 +5,17 @@ hint 'operator/conditional'
 # Conditional Branching
 pass '("?" #0# ? ?)' '?'
 pass '("?" #1# ? ?)' '?'
+pass '("?" #1# ?)' '?'
 pass '("?" #0# #100# #200#)' '#200#'
 pass '("?" #1# #100# #200#)' '#100#'
+pass '("?" #1# #100#)' '#100#'
 pass '("?" #500# #100# #200#)' '#100#'
 pass '("?" #0# "one" "two")' '"two"'
 pass '("?" #1# "one" "two")' '"one"'
+pass '("?" #1# "one")' '"one"'
 pass '("?" #0# ("-->" "var" "one") ("-->" "var" "two")) ("<--" "var")' '"two"'
 pass '("?" #1# ("-->" "var" "one") ("-->" "var" "two")) ("<--" "var")' '"one"'
+pass '("?" #1# ("-->" "var" "one")) ("<--" "var")' '"one"'
 pass '("-->" "count" #0#)
 ("?" #0# ("-->" "count" ("+" ("<--" "count") #1#)) ("-->" "count" ("+" ("<--" "count") #1#)))
 ("<--" "count")' '#1#'
@@ -21,8 +25,11 @@ pass '("-->" "count" #0#)
 
 # Argument Error
 fail '("?")' '#4#'
+fail '("?" #1#)' '#4#'
+fail '("?" #0# ?)' '#4#'
 fail '("?" ? ? ?)' '#4#'
 fail '("?" "word" ? ?)' '#4#'
+fail '("?" [#1# #2# #3#] ? ?)' '#4#'
 
 # Error Propogation
 fail '("?" ("#" "text") ? ?)' '#3#'
