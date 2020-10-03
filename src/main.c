af7eb4155eee0dd7f001b98fa22ac42beaf0c838
Jason Kaiser <wareification@programmer.net>
2020-10-03 15:06:15 -0500

    Updated program version to v0.40.3

--- src/main.c
+++ src/main.c
@@ -10,7 +10,7 @@
 #include "common.h"
 
 #define PROGRAM_NAME "snrub"
-#define PROGRAM_VERSION "v0.40.2"
+#define PROGRAM_VERSION "v0.40.3"
 
 static int complete_script(char *document);
 static int apply_script(char *document, map_t *variables);
