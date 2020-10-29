9dbbac51395de0bb2f2039d56ceeb18b0b86e463
Jason Kaiser <wareification@programmer.net>
2020-10-28 21:44:47 -0500

    Added test cases for comments.

--- test/suite/type/null.sh
+++ test/suite/type/null.sh
@@ -10,6 +10,11 @@ pass "$(printf '\011')" '?'
 pass "$(printf '\012')" '?'
 pass "$(printf '\015')" '?'
 pass "$(printf '\011\012\015\040')" '?'
+pass '`comments`' '?'
+pass '`?`' '?'
+pass '`#1#`' '?'
+pass '`[#1# \"one\"]`' '?'
+pass '`(\"#\" \"1\")`' '?'
 
 # Syntax Error
 fail "$(printf '\007')" '#1#'
