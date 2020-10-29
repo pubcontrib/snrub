e1810e51f61edf3d0e1abba7f6732c77f209391e
Jason Kaiser <wareification@programmer.net>
2020-10-28 21:44:06 -0500

    Added test cases for uncontained control characters.

--- test/suite/type/null.sh
+++ test/suite/type/null.sh
@@ -6,3 +6,10 @@ hint 'type/null'
 pass '?' '?'
 pass '' '?'
 pass ' ' '?'
+pass "$(printf '\011')" '?'
+pass "$(printf '\012')" '?'
+pass "$(printf '\015')" '?'
+pass "$(printf '\011\012\015\040')" '?'
+
+# Syntax Error
+fail "$(printf '\007')" '#1#'
