69c11e440f4154f41cc09e99a4f45876ff18fbdf
Jason Kaiser <wareification@programmer.net>
2020-10-28 21:56:57 -0500

    Removed some extra escape characters from test case input.

--- test/suite/type/null.sh
+++ test/suite/type/null.sh
@@ -13,8 +13,8 @@ pass "$(printf '\011\012\015\040')" '?'
 pass '`comments`' '?'
 pass '`?`' '?'
 pass '`#1#`' '?'
-pass '`[#1# \"one\"]`' '?'
-pass '`(\"#\" \"1\")`' '?'
+pass '`[#1# "one"]`' '?'
+pass '`("#" "1")`' '?'
 
 # Syntax Error
 fail "$(printf '\007')" '#1#'
