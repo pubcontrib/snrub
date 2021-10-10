d5c1a8cb9cbeb5ba1e7ad5aa624e409ad5d72c09
Jason Kaiser <wareification@programmer.net>
2021-10-10 12:59:33 -0500

    Updated program version to v0.72.4.

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@
 #include "common.h"
 
 #define PROGRAM_NAME "snrub"
-#define PROGRAM_VERSION "v0.72.3"
+#define PROGRAM_VERSION "v0.72.4"
 
 static int run_help(void);
 static int run_version(void);
